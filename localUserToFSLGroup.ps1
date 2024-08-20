2a76cf932f8d7b39d36f5511354eb0b709337cea
DaMemon <73153098+DaMemon@users.noreply.github.com>
2024-08-20 21:12:25 +1000

    Update localUserToFSLGroup.ps1

--- localUserToFSLGroup.ps1
+++ localUserToFSLGroup.ps1
@@ -1,2 +1,3 @@
 Add-LocalGroupMember -Group "FSLogix Profile Exclude List" -Member "3mlocaladmin"
 Add-LocalGroupMember -Group "FSLogix ODFC Exclude List" -Member "3mlocaladmin"
+Write-Output "Script Completed"
