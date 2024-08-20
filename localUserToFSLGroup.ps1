087fd322c18a26e1ebd8e3214ced8120bf076f64
DaMemon <73153098+DaMemon@users.noreply.github.com>
2024-08-20 23:45:32 +1000

    Update localUserToFSLGroup.ps1

--- localUserToFSLGroup.ps1
+++ localUserToFSLGroup.ps1
@@ -2,8 +2,8 @@ $regPath = "HKLM:\SOFTWARE\FSLogix\profiles"
 try
 {
 New-ItemProperty -Path $regPath -Name FlipFlopProfileDirectoryName -PropertyType DWORD -Value 1 -Force
-Add-LocalGroupMember -Group "FSLogix Profile Exclude List" -Member "3mlocaladmin"
-Add-LocalGroupMember -Group "FSLogix ODFC Exclude List" -Member "3mlocaladmin"
+##Add-LocalGroupMember -Group "FSLogix Profile Exclude List" -Member "3mlocaladmin"
+##Add-LocalGroupMember -Group "FSLogix ODFC Exclude List" -Member "3mlocaladmin"
 ##Write-Output "Script Completed"
 }
 catch {}
