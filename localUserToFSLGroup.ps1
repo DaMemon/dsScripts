be5862b763c14cf7653a7b0d75f8ca9f58a66ea7
DaMemon <73153098+DaMemon@users.noreply.github.com>
2024-08-21 12:35:18 +1000

    Update localUserToFSLGroup.ps1
    final script for the first custom extension

--- localUserToFSLGroup.ps1
+++ localUserToFSLGroup.ps1
@@ -1,12 +1,14 @@
 $nowTime = $(get-date -f dd-MM-yy_hh-mm-ss)
-Start-Transcript -Path C:\Windows\Temp\sfc_$nowTime.log 
-$regPath = "HKCU:\Software\ITProCloud\WVDAdmin"
+$startTime = $(get-date -f dd-MM-yy_dddd_hh-mm-ss)
+Start-Transcript -Path C:\Windows\Temp\sfc_$nowTime.log
+Write-host "Script Started - $($startTime)"
+##$regPath = "HKLM:\SOFTWARE\FSLogix\profiles"
 try
 {
-New-ItemProperty -Path $regPath -Name FlipFlopProfileDirectoryName -PropertyType DWORD -Value 1 -Force #| Out-Default -Transcript
-##Add-LocalGroupMember -Group "FSLogix Profile Exclude List" -Member "3mlocaladmin"
-##Add-LocalGroupMember -Group "FSLogix ODFC Exclude List" -Member "3mlocaladmin"
-Write-Information "Script Completed" ##-Verbose
+#New-ItemProperty -Path $regPath -Name FlipFlopProfileDirectoryName -PropertyType DWORD -Value 1 -Force #| Out-Default -Transcript
+Add-LocalGroupMember -Group "FSLogix Profile Exclude List" -Member "3mlocaladmin"
+Add-LocalGroupMember -Group "FSLogix ODFC Exclude List" -Member "3mlocaladmin"
 }
 catch {}
+Write-Information "Script Completed" ##-Verbose
 Stop-Transcript
