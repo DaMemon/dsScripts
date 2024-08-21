51db5be3cdeacf0b7f36db1138e258aa89e25140
DaMemon <73153098+DaMemon@users.noreply.github.com>
2024-08-21 12:19:44 +1000

    Update localUserToFSLGroup.ps1

--- localUserToFSLGroup.ps1
+++ localUserToFSLGroup.ps1
@@ -3,10 +3,10 @@ Start-Transcript -Path C:\Windows\Temp\sfc_$nowTime.log
 $regPath = "HKCU:\Software\ITProCloud\WVDAdmin"
 try
 {
-New-ItemProperty -Path $regPath -Name FlipFlopProfileDirectoryName -PropertyType DWORD -Value 1 -Force | Out-Default -Transcript
+New-ItemProperty -Path $regPath -Name FlipFlopProfileDirectoryName -PropertyType DWORD -Value 1 -Force #| Out-Default -Transcript
 ##Add-LocalGroupMember -Group "FSLogix Profile Exclude List" -Member "3mlocaladmin"
 ##Add-LocalGroupMember -Group "FSLogix ODFC Exclude List" -Member "3mlocaladmin"
-Write-Output "Script Completed" ##-Verbose
+Write-Information "Script Completed" ##-Verbose
 }
 catch {}
 Stop-Transcript
