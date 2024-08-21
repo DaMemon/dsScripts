03afceee1c2d1b6d843d4b6365042e7d16503c3a
DaMemon <73153098+DaMemon@users.noreply.github.com>
2024-08-21 12:00:37 +1000

    Update localUserToFSLGroup.ps1

--- localUserToFSLGroup.ps1
+++ localUserToFSLGroup.ps1
@@ -1,12 +1,12 @@
 $nowTime = $(get-date -f dd-MM-yy_hh-mm-ss)
 Start-Transcript -Path C:\Windows\Temp\sfc_$nowTime.log 
-#$regPath = "HKLM:\SOFTWARE\FSLogix\profiles"
+$regPath = "HKLM:\SOFTWARE\FSLogix\profiles"
 try
 {
-#New-ItemProperty -Path $regPath -Name FlipFlopProfileDirectoryName -PropertyType DWORD -Value 1 -Force
+New-ItemProperty -Path $regPath -Name FlipFlopProfileDirectoryName -PropertyType DWORD -Value 1 -Force | Out-Default -Transcript
 ##Add-LocalGroupMember -Group "FSLogix Profile Exclude List" -Member "3mlocaladmin"
 ##Add-LocalGroupMember -Group "FSLogix ODFC Exclude List" -Member "3mlocaladmin"
-Write-Output "Script Completed" -verbose
+Write-Information "Script Completed" ##-Verbose
 }
 catch {}
 Stop-Transcript
